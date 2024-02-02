bc3c501f6c231436a9499dd52ec99bc99117eb41
EJCarron <ed.james.carron@gmail.com>
2024-02-02 07:28:24 +0000

    added savecsvs command

--- src/scripts/chi.py
+++ src/scripts/chi.py
@@ -95,3 +95,30 @@ def loadjsoncreategraph(path):
 
     with graphDB_Driver.session() as graphDB_Session:
         graphDB_Session.run(cypher)
+
+
+@chi.command()
+@click.option("--officer_ids", "-oid", multiple=True,
+              prompt="the Companies House ID of the person that you want to find the "
+                     "network of.",
+              help="Can be found in the url of the person\'s Companies House profile.", default=[''])
+@click.option("--layers", prompt="The number of times you want to expand the network outwards", default=1,
+              help="Warning networks can quickly become extremely large, not recommended to exceed 2 to 3 layers.")
+@click.option("--appointments_limit", default=100,
+              prompt="The limit for the number of appointments the program will pull. If the number exceeds the limit "
+                     "the officer will still be pulled but without it\' related companies and appointments.",
+              help="If no limit wanted set to -1. NOT ADVISED some officers can have extremely large number of "
+                   "appointments. "
+              )
+@click.option("--path", "-p", prompt="path to the save directory (it must already exist).")
+def savecsvs(officer_ids, layers, appointments_limit, path):
+    helpers.check_and_init_config()
+    requests_counter = 0
+
+    network, requests_counter = Network.start(officer_ids=officer_ids,
+                                              requests_count=requests_counter,
+                                              appointments_limit=appointments_limit)
+
+    network.expand_network(requests_count=requests_counter, layers=layers, appointments_limit=appointments_limit)
+
+    network.save_csvs(directory_path=path)
